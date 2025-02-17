4aab1383a12876bdd08465cf1910c63fee03a0fe
Ochieng Paul <ochiengpaul442@gmail.com>
2025-02-17 17:18:14 +0300

    updates

--- dawa-website/src/app/server/my-shop/api.tsx
+++ dawa-website/src/app/server/my-shop/api.tsx
@@ -1,10 +1,10 @@
-import { openApiClient, secureApiClient } from '@/utils/apiClient';
+import { openApiClient } from '@/utils/apiClient';
 
 // get myshop data take in a body
 export const getShopData = async (
   url: string,
   body: { user_id: number },
 ): Promise<any> => {
-  const response = await secureApiClient.post(url, body);
+  const response = await openApiClient.post(url, body);
   return response.data;
 };
