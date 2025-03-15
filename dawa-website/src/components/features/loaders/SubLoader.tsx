7aaa3e1f846b0d586763afcf7ec20cb9252ea865
Paul Ochieng Levi <ochieng442@outlook.com>
2025-03-15 23:45:53 +0300

    updates

--- dawa-website/src/components/features/loaders/SubLoader.tsx
+++ dawa-website/src/components/features/loaders/SubLoader.tsx
@@ -1,7 +1,8 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-auto py-16">
+    <div className="flex flex-col justify-center items-center h-auto py-16">
       <div className="SpinnerLoader"></div>
+      <p className="mt-4 text-center">Please Wait...</p>
     </div>
   );
 };
